f56b9a83bd98f518580ebc39faf9ceb650d906aa
Iván-Modash <ivan@modash.io>
2025-03-26 09:34:44 +0100

    Updated config

--- src/config/wedding.config.ts
+++ src/config/wedding.config.ts
@@ -2,16 +2,16 @@ import { WeddingConfigType } from "@/interfaces/config"
 
 const weddingConfig: WeddingConfigType = {
     people: {
-        bride: { firstName: 'Jane', lastName: 'Doe' },
-        groom: { firstName: 'John', lastName: 'Smith' }
+        bride: { firstName: 'Aitana', lastName: 'Doe' },
+        groom: { firstName: 'Iván', lastName: 'Smith' }
     },
     date: {
-        date: '2026-01-31',
-        time: '16:30' // Optional
+        date: '2025-07-05',
+        time: '13:00' // Optional
     },
     location: {
         title: 'Bodega Viñedos Ruiz Jiménez',
-        address: 'Carretera Comarcal, LR-115, Km 43, 5, 26559 Aldeanueva de Ebro, La Rioja',
+        address: 'Carretera Comarcal, LR-115, Km 43, 5, Aldeanueva de Ebro',
         link: 'https://maps.app.goo.gl/KQtCr3DVonad7y72A',
         latLng: '42.2228946, -1.9030969'
     }
