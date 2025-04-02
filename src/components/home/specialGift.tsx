39abaf94df1faf6944e0ddc31e5b3096d0d4a1bb
Iván-Modash <ivan@modash.io>
2025-04-02 23:41:24 +0200

    Added new component below the FAQ

--- src/components/home/specialGift.tsx
+++ src/components/home/specialGift.tsx
@@ -1,58 +1,71 @@
 import React, { FC } from 'react'
 import Image from 'next/image'
-import { Card, CardActionArea, Box, Typography } from '@mui/material'
+import { Card, CardActionArea, Box, Typography, Container, Grid } from '@mui/material'
 import theme from '@/config/theme'
 
-const SpecialGiftInfo: FC = () => {
+const HomeSpecialGift: FC = () => {
   return (
-    <Card
-      elevation={4}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 3,
-        backgroundColor: theme.palette.primary.dark,
-        width: 500,
-        height: 700,
-        mx: 'auto',
-        my: 3,
-        overflow: 'hidden',
-      }}
+    <Box
+      id="home-specialgift"
+      sx={{ pt: { xs: 2, md: 4 }, backgroundColor: 'background.paper', pb: 4, overflowX: 'hidden' }}
     >
-      <CardActionArea
-        sx={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          p: 3,
-          height: '100%',
-          overflow: 'hidden'
-        }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="/images/home/ivan_y_aitana_bus_combined_2.png"
-      >
-        <Typography variant="h2" align="center" color="primary.contrastText" sx={{ my: 1 }}>
-          Un regalo especial 
-        </Typography>
-
-        <Box sx={{ 
-          borderRadius: 5, 
-          overflow: 'hidden', 
-          cursor: 'pointer', 
-          display: 'flex', 
-          justifyContent: 'center', 
-          width: '100%',
-          flex: 1
-        }}>
-          <Image alt="Dress code" src="/images/home/ivan_y_aitana_recuerdo_especial.png" width={350} height={500} style={{ objectFit: 'contain' }} />
-        </Box>
-      </CardActionArea>
-    </Card>
+      <Container maxWidth="lg" sx={{ width: '100%', mx: 'auto' }}>
+        <Grid
+          container
+          sx={{ justifyContent: 'center', alignItems: 'center' }}
+          spacing={{ xs: 4, md: 3 }}
+          columns={{ xs: 2, sm: 2, md: 12 }}
+          rowSpacing={6}
+        >
+          <Grid item xs={2} sm={12} md={12} key="special-gift">
+            <Card
+              elevation={4}
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 3,
+                backgroundColor: theme.palette.primary.dark,
+                width: 500,
+                height: 700,
+                mx: 'auto',
+                my: 3,
+                overflow: 'hidden',
+              }}
+            >
+              <CardActionArea
+                sx={{ 
+                  display: 'flex', 
+                  flexDirection: 'column', 
+                  alignItems: 'center', 
+                  justifyContent: 'center', 
+                  p: 3,
+                  height: '100%',
+                  overflow: 'hidden'
+                }}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="/images/home/ivan_y_aitana_recuerdo_especial_mod.png"
+              >
+                <Box sx={{ 
+                  borderRadius: 5, 
+                  overflow: 'hidden', 
+                  cursor: 'pointer', 
+                  display: 'flex', 
+                  justifyContent: 'center', 
+                  width: '100%',
+                  flex: 1
+                }}>
+                  <Image alt="Regalo especial" src="/images/home/ivan_y_aitana_recuerdo_especial_mod.png" width={450} height={650} style={{ objectFit: 'contain' }} />
+                </Box>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
 
-export default SpecialGiftInfo
+export default HomeSpecialGift
